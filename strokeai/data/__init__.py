ed24cc1aac4dd0376383a55b2e2f029982531a89
Hui Liu <huiliu.liu@gmail.com>
2020-10-15 12:31:20 +0800

    data

---
+++ strokeai/data/__init__.py
@@ -0,0 +1,12 @@
+"""
+    Program: strokeai/itkprocessing
+        
+    A python wrapped medical imaging processing tools for stroke
+
+    file name: __init__.py
+    date of creation: 2020-07-30
+    date of modification: 2020-08-01
+    Author: Liu Hui (huiliu.liu@gmail.com)
+    purpose: 
+        
+"""
