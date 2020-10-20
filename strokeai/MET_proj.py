e004ea606864283acc6d19505c643539606d0a68
hliudev <hliudev@email.com>
2020-10-21 00:52:13 +0800

    met_proj

---
+++ strokeai/MET_proj.py
@@ -0,0 +1,79 @@
+
+import os
+import pandas as pd
+import SimpleITK as sitk
+
+from strokeai.itkprocessing.resampling import resample_sitk_image_by_reference
+
+#------------------------------------------------------------------------------
+#
+# test purpose
+#
+#------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    project_root = 'D:\\data\\MET_Proj'
+    nii_root = os.path.join(project_root, 'niix')
+    roi_root = os.path.join(project_root, 'roi')
+
+    patient_names = os.listdir(nii_root)
+    roi_patient_names = os.listdir(roi_root)
+    roi_patient_names_b = [roi_patient_name.replace('_', '').replace(' ', '') for roi_patient_name in roi_patient_names]
+
+    # generate radiomics csv
+    data_dict = {'ID': [], 'Image': [], 'Mask': [], 'Type': []}
+
+    for patient_name in patient_names:
+        pb = patient_name.replace('_', '').replace(' ', '')
+        roi_i = 0
+        for i, roi_b in enumerate(roi_patient_names_b):
+            if roi_b == pb: roi_i = i
+
+        print('working on', pb, ' ', roi_patient_names_b[roi_i])
+
+        mk_file = os.path.join(nii_root, patient_name, 'mk.nii.gz')
+        if not os.path.exists(mk_file): continue
+
+        roi_1_file = os.path.join(roi_root, roi_patient_names[roi_i], 'ROI_1.nii')
+        roi_ref_file = os.path.join(roi_root, roi_patient_names[roi_i], 'ROI_ref.nii')
+
+        sitk_img_roi1 = sitk.ReadImage(roi_1_file)
+
+        # resample mk
+        sitk_img_mk = sitk.ReadImage(mk_file)
+        mk_r_file =  os.path.join(nii_root, patient_name, 'mk_r.nii.gz')
+        if not os.path.exists(mk_r_file):
+            sitk_img_mk_r = resample_sitk_image_by_reference(sitk_img_roi1, sitk_img_mk)
+            sitk.WriteImage(sitk_img_mk_r, mk_r_file)
+
+        # resample md
+        md_file = os.path.join(nii_root, patient_name, 'md.nii.gz')
+        sitk_img_md = sitk.ReadImage(md_file)
+        md_r_file = os.path.join(nii_root, patient_name, 'md_r.nii.gz')
+        if not os.path.exists(md_r_file):
+            sitk_img_md_r = resample_sitk_image_by_reference(sitk_img_roi1, sitk_img_md)
+            sitk.WriteImage(sitk_img_md_r, md_r_file)
+
+        data_dict['ID'].append(patient_name)
+        data_dict['Image'].append(mk_r_file)
+        data_dict['Mask'].append(roi_1_file)
+        data_dict['Type'].append('mk_roi_1')
+
+        data_dict['ID'].append(patient_name)
+        data_dict['Image'].append(mk_r_file)
+        data_dict['Mask'].append(roi_ref_file)
+        data_dict['Type'].append('mk_roi_ref')
+
+        data_dict['ID'].append(patient_name)
+        data_dict['Image'].append(md_r_file)
+        data_dict['Mask'].append(roi_1_file)
+        data_dict['Type'].append('md_roi_1')
+
+        data_dict['ID'].append(patient_name)
+        data_dict['Image'].append(md_r_file)
+        data_dict['Mask'].append(roi_ref_file)
+        data_dict['Type'].append('md_roi_ref')
+
+    df = pd.DataFrame(data_dict)
+    df.to_csv(os.path.join(project_root, 'radiomics.csv'))
+    df.to_csv(os.path.join(project_root, 'radiomics.txt'))
