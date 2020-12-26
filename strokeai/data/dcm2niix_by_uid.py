be2c8b7f83cd0282839e7c1fcd2b2470704d5935
LiuHui <LiuHui@LiuHuis-MacBook-Pro.local>
2020-12-26 15:07:43 +0800

    v1.0.1

--- strokeai/data/dcm2niix_by_uid.py
+++ strokeai/data/dcm2niix_by_uid.py
@@ -11,6 +11,7 @@
 
 """
 import os
+import re
 import shutil
 import pydicom
 import pandas as pd
@@ -19,22 +20,27 @@ from pydicom.misc import is_dicom
 
 #------------------------------------------------------------------------------
 #
-def generate_info(dcm_root):
+def generate_dcm_index(dcm_root):
     """
     :param dcm_root:
     :return:
     """
-    dcmtags = {'SeriesInstanceUID': [], 'SeriesDescription': [], 'PatientName': [],
-               'PatientID': [], 'Modality': [], 'StudyDateTime': [],
+    dcmtags = {'SeriesInstanceUID': [], 'SeriesDescription': [],
+               'PatientName': [], 'PatientID': [],
+               'Modality': [], 'StudyDateTime': [],
                'Spacing_X': [], 'Spacing_Y': [], 'Spacing_Z': [],
                'AKAFileName': [], 'SubRoot': []}
     for sub_root, _, files in os.walk(dcm_root):
         if len(files) <= 0: continue
+        print('working on %s ...'%(sub_root))
         for file in files:
             dcm_file = os.path.join(sub_root, file)
             if dcm_file.endswith('.json'): continue
             if dcm_file.endswith('.nii'): continue
             if dcm_file.endswith('.nii.gz'): continue
+            if dcm_file.endswith('.bval'): continue
+            if dcm_file.endswith('.bvec'): continue
+            if dcm_file.endswith('ICOMDIR'): continue
             if not is_dicom(dcm_file): continue
             ds = pydicom.read_file(dcm_file, stop_before_pixels=True, force=True)
             if ds.SeriesInstanceUID in dcmtags['SeriesInstanceUID']: continue
@@ -52,7 +58,7 @@ def generate_info(dcm_root):
                 dcmtags['StudyDateTime'].append(studydatetime)
                 dcmtags['Modality'].append(modality)
                 dcmtags['AKAFileName'].append('')
-                dcmtags['SubRoot'].append(sub_root)
+                dcmtags['SubRoot'].append(sub_root.replace(dcm_root, '..'))
             except: continue
             try:
                 spacing = (float(ds.PixelSpacing[0]), float(ds.PixelSpacing[1]), float(ds.SliceThickness))
@@ -64,6 +70,37 @@ def generate_info(dcm_root):
                 dcmtags['Spacing_Y'].append('NA')
                 dcmtags['Spacing_Z'].append('NA')
     return pd.DataFrame(dcmtags)
+#------------------------------------------------------------------------------
+#
+def organize_dcm_byuid(src_dcm_root, target_dcm_root):
+    """
+    :param src_dcm_root:
+    :param target_dcm_root:
+    :return:
+    """
+    _pattern = re.compile('\w')
+    for sub_root, _, files in os.walk(src_dcm_root):
+        if len(files) <= 0: continue
+        print('working on %s ...' % (sub_root))
+        for file in files:
+            dcm_file = os.path.join(sub_root, file)
+            if dcm_file.endswith('.json'): continue
+            if dcm_file.endswith('.nii'): continue
+            if dcm_file.endswith('.nii.gz'): continue
+            if dcm_file.endswith('.bval'): continue
+            if dcm_file.endswith('.bvec'): continue
+            if dcm_file.endswith('ICOMDIR'): continue
+            if not is_dicom(dcm_file): continue
+            ds = pydicom.read_file(dcm_file, stop_before_pixels=True, force=True)
+            series_uid = str(ds.SeriesInstanceUID)
+            study_uid = str(ds.StudyDate + ds.StudyTime) + '.' + str(ds.StudyInstanceUID)
+            _patient_name = _pattern.match(str(ds.PatientName)).string.upper().replace(' ', '')
+            patient_uid = 'PID.' + str(ds.PatientID) + '.PNAME.' + _patient_name.replace('_', '')
+            filename = str(ds.SOPInstanceUID) + '.dcm'
+            file_root = os.path.join(target_dcm_root, patient_uid, study_uid, series_uid)
+            os.makedirs(file_root, exist_ok=True)
+            shutil.copyfile(dcm_file, os.path.join(file_root, filename))
+    return
 #------------------------------------------------------------------------------
 #
 def cp_nii_by_info(tmp_folder, nii_folder, info_xlsx_file):
@@ -114,20 +151,24 @@ def cp_nii_by_info(tmp_folder, nii_folder, info_xlsx_file):
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    dcmroot = 'D:\\DICOMDB\\Shanghai_6th_Hospital\\001_Stroke_DWI\\001_dicom_2'
+    dcmroot = '/Users/LiuHui/DicomDB/Shanghai_6th_Hospital/Stroke/WangFeng'
+    dcmroot_2 = '/Users/LiuHui/DicomDB/Shanghai_6th_Hospital/Stroke/dcm'
     tmproot = 'D:\\DICOMDB\\Shanghai_6th_Hospital\\001_Stroke_DWI\\tmp'
     niiroot = 'D:\\DICOMDB\\Shanghai_6th_Hospital\\001_Stroke_DWI\\002_niix_2'
 
+    #organize_dcm_byuid(dcmroot, dcmroot_2)
+    df = generate_dcm_index(dcmroot_2)
+    df.to_excel(os.path.join(dcmroot_2, 'dcm_index.xlsx'))
     # step 1: dump dcm tags into dump.xlsx
-
-    sub_dcmroots = [os.path.join(dcmroot, o) for o in os.listdir(dcmroot) if os.path.isdir(os.path.join(dcmroot, o))]
-    dfs = []
-    for sub_dcmroot in sub_dcmroots:
-        if sub_dcmroot.endswith('.xlsx'): continue
-        print('working on %s'%os.path.basename(sub_dcmroot))
-        dfs.append(generate_info(sub_dcmroot))
-    df = pd.concat(dfs, axis=0)
-    df.to_excel(os.path.join(dcmroot, 'dump.xlsx'))
+    #sub_dcmroots = [os.path.join(dcmroot, o) for o in os.listdir(dcmroot) if os.path.isdir(os.path.join(dcmroot, o))]
+    #dfs = []
+    #for sub_dcmroot in sub_dcmroots:
+    #    if sub_dcmroot.endswith('.xlsx'): continue
+    #    print('working on %s'%os.path.basename(sub_dcmroot))
+    #    dfs.append(generate_dcm_index(sub_dcmroot))
+    # df = pd.concat(dfs, axis=0)
+    #df = generate_dcm_index(dcmroot)
+    #df.to_excel(os.path.join(dcmroot, 'dcm_index.xlsx'))
 
 
     # step 2: update dump.xlsx
